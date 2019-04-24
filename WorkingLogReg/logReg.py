1b32daf669d522d61451d67b0ca076a6ab782e4d
Quinotn <q.denman@student.maastrichtuniversity.nl>
2019-04-24 14:27:29 +0200

    changes

--- WorkingLogReg/logReg.py
+++ WorkingLogReg/logReg.py
@@ -4,6 +4,57 @@ import math
 import matplotlib.pyplot as plt
 from sklearn import preprocessing
 
+def forwardPass(x):
+    inp = np.insert(x, 0, 1, axis=0)
+    a = np.dot(weights, inp)
+    if (1 + math.exp(-a)) != 0:
+        sigmoid = 1 / (1 + math.exp(-a))
+    else:
+        sigmoid = eps
+    return sigmoid
+
+
+def regTerm(i, lamReg, reg):
+    if i != 0 & reg == True:
+        return lamReg / 2 * np.sum(weights ** 2)
+    return 0
+
+
+def trainNN(regularization, learningRate, featuresToDelete, samples):
+
+    data = np.delete(samples, featuresToDelete, axis=1)
+    data = np.random.permutation(data)
+    data_train = data[:int(0.8 * len(data))]
+    # print(data_train)
+    data_test = data[int(0.8 * len(data)):]
+    samples = data[:, :2]
+    targets = data[:, 2]
+    epochs = 0
+    converged = False
+    while not converged:
+        epochs += 1
+        for j in range(len(samples)):
+            out = forwardPass(samples[j])
+            inpTemp = np.insert(samples[j], 0, 1, axis=0)
+            for r in range(len(weights)):
+                gradient = (targets[j] - out) * inpTemp[r]
+                weights[r] -= learningRate * -gradient + regTerm(r, 0.001, regularization)
+
+            pred = np.clip(out, eps, 1 - eps)
+            loss = -targets[j] * math.log(pred) - (1 - targets[j]) * math.log(1 - pred)
+        # if epochs%100 == 0:
+        #     print(f'Error in epoch {epochs}: {loss}')
+        if np.abs(loss) < 0.005:
+            converged = True
+            print(f'Converged after {epochs} epochs.')
+    test = data_test[:, :2]
+    testTarget = data_test[:, 2]
+    sm = 0
+    for q in range(len(test)):
+        sm += (abs(testTarget[q]-forwardPass(test[q])))
+        print(f'difference {sm}')
+        # print(f'Target: {testTarget[q]} prediction: {forwardPass(test[q])}')
+
 
 df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
 df.head()
@@ -13,49 +64,40 @@ samples = df.copy()
 samples[4][samples[4] == classes[0]] = 0
 samples[4][samples[4] == classes[1]] = 1
 samples = preprocessing.MinMaxScaler().fit_transform(samples)
-# print(samples)
-featuresToDelete = [1, 3]
-data = np.delete(samples, featuresToDelete, axis=1)
-data = np.random.permutation(data)
-data_train = data[:int(0.8*len(data))]
-# print(data_train)
-data_test = data[int(0.8*len(data)):]
-samples = data[:, :2]
-targets = data[:, 2]
-print(targets.shape)
-print(samples.shape)
-# print(samples)
-# print(samples.shape)
-# print(targets)
-# print(targets.shape)
+samples2 = samples.copy()
 weights = np.random.randn(3)
-lr = 0.01
-# print(w)
-lossTrace = []
+weights2 = weights.copy()
+eps = 1e-15
+trainNN(True, 0.1, [1, 3], samples)
+weights = weights2
+samples = samples2
+trainNN(True, 0.1, [0, 3], samples)
+weights = weights2
+samples = samples2
+trainNN(True, 0.1, [2, 3], samples)
+weights = weights2
+samples = samples2
+trainNN(True, 0.1, [1, 2], samples)
 
-def forwardPass(x):
-    inp = x
-    inp = np.insert(inp, 0, 1, axis=0)
-    a = np.sum(weights * inp)
-    sigmoidal = 1 / (1 + math.exp(-a))
-    return sigmoidal
 
-for i in range(3000):
 
-    for j in range(len(samples)):
-        out = forwardPass(samples[j])
-        inp2 = np.insert(samples[j], 0, 1, axis=0)
-        # if j < 50: print(a), print(out)
-        for r in range(weights.shape[0]):
-            gradient = (targets[j] - out) * inp2[r]
-            weights[r] -= lr * -gradient
+def plotConverge(samples, features):
+    step_size = .01
+    x_min, x_max = samples[:, features[0]].min() - 1, samples[:, features[0]].max() + 1
+    y_min, y_max = samples[:, features[1]].min() - 1, samples[:, features[1]].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, step_size),
+                            np.arange(y_min, y_max, step_size))
+    Z = np.array(list(map(forwardPass, np.c_[xx.ravel(), yy.ravel()])))
 
-        prediction = 1 - 1e-15 if out == 1 else out
-        prediction = 1e-15 if out == 0 else out
-        loss = -targets[j] * math.log(prediction) - (1 - targets[j]) * math.log(1 - prediction)
-        lossTrace.append(loss)
-print(loss)
-test = data_test[:, :2]
-testTarget = data_test[:, 2]
-for q in range(len(test)):
-    print(f'Target: {testTarget[q]} prediction: {forwardPass(test[q])}')
+    Z = Z.reshape(xx.shape)
+    plt.contour(xx, yy, Z, cmap="hot", levels=1)
+
+    # plot data points
+    plt.scatter(x=samples[features[0]], y=samples[features[1]], cmap="tab10")
+
+    # plt.title(f"Features {stats[2][0]} and {stats[2][1]}; Accuracy: {stats[0]}, Loss: {'%.2E' % stats[1]}")
+
+
+    plt.show()
+
+#plotConverge(samples, [0, 2])
